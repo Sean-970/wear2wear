029fc98108243b0df77fa1b17b33c230c27a99d2
Sean <xiana1019@gmail.com>
2024-10-30 18:03:44 +0800

    1030

--- FinalS1/src/main/java/tw/Final/FinalS1/repository/CartItemsRepository.java
+++ FinalS1/src/main/java/tw/Final/FinalS1/repository/CartItemsRepository.java
@@ -11,7 +11,7 @@ import tw.Final.FinalS1.model.CartModel;
 @Repository
 public interface CartItemsRepository extends JpaRepository<CartItemsModel, Integer> {
 
-	//新增此方法來根據 cartId 刪除 CartItemss
+	//新增此方法來根據 cartId 刪除 CartItemsssa
 	void deleteByCartId(Long cartId);
 
 	List<CartItemsModel> findByCartId(Long cartId);
